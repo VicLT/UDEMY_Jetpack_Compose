ec1a08cff7f5a68247699ffac62681302c34de16
Victor LT <victor_lamas@hotmail.com>
2025-07-25 20:03:05 +0200

    Badge

--- app/src/main/java/udemy/victorlamas/myfirstcomposeapp/components/Badge.kt
+++ app/src/main/java/udemy/victorlamas/myfirstcomposeapp/components/Badge.kt
@@ -1,2 +1,35 @@
 package udemy.victorlamas.myfirstcomposeapp.components
 
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import udemy.victorlamas.myfirstcomposeapp.R
+
+@Composable
+fun MyBadge(modifier: Modifier = Modifier) {
+    Badge(
+        modifier = modifier,
+        contentColor = Color.Blue,
+        containerColor = Color.Green
+    ) {
+        Text("3")
+    }
+}
+
+@Composable
+fun MyBadgeBox(modifier: Modifier = Modifier) {
+    BadgedBox(
+        modifier = modifier,
+        badge = { MyBadge() }
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_senderista),
+            contentDescription = null
+        )
+    }
+}
---
+++ app/src/main/java/udemy/victorlamas/myfirstcomposeapp/components/Divider.kt
@@ -0,0 +1,2 @@
+package udemy.victorlamas.myfirstcomposeapp.components
+
